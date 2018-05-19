8457b2b2a3a2345d2167449508c06b69196d5d82
Anam Dang <39315290+anamdang@users.noreply.github.com>
2018-05-18 21:10:01 -0400

    Add files via upload

---
+++ soen6611groupB/R/soen6611.input.R
@@ -0,0 +1,4 @@
+# To generate random numbers without replacement from 0 to 100:
+input_array <- sample(0:100, 100, replace=FALSE)
+
+
---
+++ soen6611groupB/R/soen6611.output.R
@@ -0,0 +1,10 @@
+#Test scripts for the output
+soen6611.max(input_array)
+
+soen6611.min(input_array)
+
+soen6611.mean(input_array)
+
+soen6611.mode(input_array)
+
+soen6611.std(input_array)
